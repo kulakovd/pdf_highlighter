c219926255b01efe96defe48c94a8999e91e9d64
mikhailz <zisman.mikhail@gmail.com>
2020-05-24 23:44:29 +0300

    added splitter

--- packages/example/src/App.js
+++ packages/example/src/App.js
@@ -1,16 +1,18 @@
 // @flow
 
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import URLSearchParams from "url-search-params";
+import SplitterLayout from 'react-splitter-layout';
+import 'react-splitter-layout/lib/index.css';
 
 import {
-  PdfLoader,
-  PdfHighlighter,
-  Tip,
-  Highlight,
-  Popup,
-  AreaHighlight
+    PdfLoader,
+    PdfHighlighter,
+    Tip,
+    Highlight,
+    Popup,
+    AreaHighlight
 } from "react-pdf-highlighter";
 
 import testHighlights from "./test-highlights";
@@ -19,8 +21,8 @@ import Spinner from "./Spinner";
 import Sidebar from "./Sidebar";
 
 import type {
-  T_Highlight,
-  T_NewHighlight
+    T_Highlight,
+    T_NewHighlight
 } from "react-pdf-highlighter/src/types";
 
 import "./style/App.css";
@@ -30,24 +32,24 @@ type T_ManuscriptHighlight = T_Highlight;
 type Props = {};
 
 type State = {
-  highlights: Array<T_ManuscriptHighlight>
+    highlights: Array<T_ManuscriptHighlight>
 };
 
 const getNextId = () => String(Math.random()).slice(2);
 
 const parseIdFromHash = () =>
-  document.location.hash.slice("#highlight-".length);
+    document.location.hash.slice("#highlight-".length);
 
 const resetHash = () => {
-  document.location.hash = "";
+    document.location.hash = "";
 };
 
-const HighlightPopup = ({ comment }) =>
-  comment.text ? (
-    <div className="Highlight__popup">
-      {comment.emoji} {comment.text}
-    </div>
-  ) : null;
+const HighlightPopup = ({comment}) =>
+    comment.text ? (
+        <div className="Highlight__popup">
+            {comment.emoji} {comment.text}
+        </div>
+    ) : null;
 
 const DEFAULT_URL = "https://arxiv.org/pdf/1708.08021.pdf";
 
@@ -58,177 +60,179 @@ let item = localStorage.getItem(url);
 
 class App extends Component<Props, State> {
 
-  state = {
-    highlights: item ? JSON.parse(item) ? [...JSON.parse(item)] : [] : [],
-    rotate: false
-  };
+    state = {
+        highlights: item ? JSON.parse(item) ? [...JSON.parse(item)] : [] : [],
+        rotate: false
+    };
 
-  state: State;
+    state: State;
 
-  resetHighlights = () => {
-    this.setState({
-      ...this.state, highlights: []
-    });
-    console.log(this.state)
-  };
+    resetHighlights = () => {
+        this.setState({
+            ...this.state, highlights: []
+        });
+        console.log(this.state)
+    };
 
-  rotate = () => {
-    this.setState({
-      ...this.state, rotate: !this.state.rotate
-    });
-    console.log(this.state)
-  };
+    rotate = () => {
+        this.setState({
+            ...this.state, rotate: !this.state.rotate
+        });
+        console.log(this.state)
+    };
 
-  scrollViewerTo = (highlight: any) => {
-  };
+    scrollViewerTo = (highlight: any) => {
+    };
 
-  scrollToHighlightFromHash = () => {
-    const highlight = this.getHighlightById(parseIdFromHash());
+    scrollToHighlightFromHash = () => {
+        const highlight = this.getHighlightById(parseIdFromHash());
 
-    if (highlight) {
-      this.scrollViewerTo(highlight);
+        if (highlight) {
+            this.scrollViewerTo(highlight);
+        }
+    };
+
+    componentDidMount() {
+        window.addEventListener(
+            "hashchange",
+            this.scrollToHighlightFromHash,
+            false
+        );
     }
-  };
 
-  componentDidMount() {
-    window.addEventListener(
-      "hashchange",
-      this.scrollToHighlightFromHash,
-      false
-    );
-  }
+    getHighlightById(id: string) {
+        const {highlights} = this.state;
 
-  getHighlightById(id: string) {
-    const { highlights } = this.state;
+        return highlights.find(highlight => highlight.id === id);
+    }
 
-    return highlights.find(highlight => highlight.id === id);
-  }
+    addHighlight(highlight: T_NewHighlight) {
+        const {highlights} = this.state;
 
-  addHighlight(highlight: T_NewHighlight) {
-    const { highlights } = this.state;
+        console.log("Saving highlight", highlight);
 
-    console.log("Saving highlight", highlight);
+        localStorage.setItem(url, JSON.stringify(highlight));
+        this.setState({
+            highlights: [{...highlight, id: getNextId()}, ...highlights]
+        });
+    }
 
-    localStorage.setItem(url, JSON.stringify(highlight));
-    this.setState({
-      highlights: [{ ...highlight, id: getNextId() }, ...highlights]
-    });
-  }
+    updateHighlight(highlightId: string, position: Object, content: Object) {
+        console.log("Updating highlight", highlightId, position, content);
 
-  updateHighlight(highlightId: string, position: Object, content: Object) {
-    console.log("Updating highlight", highlightId, position, content);
+        this.setState({
+            highlights: this.state.highlights.map(h => {
+                return h.id === highlightId
+                    ? {
+                        ...h,
+                        position: {...h.position, ...position},
+                        content: {...h.content, ...content}
+                    }
+                    : h;
+            })
+        });
+    }
 
-    this.setState({
-      highlights: this.state.highlights.map(h => {
-        return h.id === highlightId
-          ? {
-            ...h,
-            position: { ...h.position, ...position },
-            content: { ...h.content, ...content }
-          }
-          : h;
-      })
-    });
-  }
+    render() {
+        const {highlights, rotate} = this.state;
+        localStorage.setItem(url, highlights);
+        return (
+            <div className="App" style={{display: "flex", height: "100vh"}}>
+                <SplitterLayout>
+                    <div
+                        style={{
+                            height: "100vh",
+                            width: "75vw",
+                            overflowY: "scroll",
+                            position: "relative"
+                        }}
+                    >
+                        <PdfLoader url={url} beforeLoad={<Spinner/>}>
+                            {pdfDocument => (
+                                <PdfHighlighter
+                                    pdfDocument={pdfDocument}
+                                    rotate={rotate}
+                                    enableAreaSelection={event => event.altKey}
+                                    onScrollChange={resetHash}
+                                    scrollRef={scrollTo => {
+                                        this.scrollViewerTo = scrollTo;
 
-  render() {
-    const { highlights, rotate } = this.state;
-    localStorage.setItem(url, highlights);
-    return (
-      <div className="App" style={{ display: "flex", height: "100vh" }}>
-        <div
-          style={{
-            height: "100vh",
-            width: "75vw",
-            overflowY: "scroll",
-            position: "relative"
-          }}
-        >
-          <PdfLoader url={url} beforeLoad={<Spinner/>}>
-            {pdfDocument => (
-              <PdfHighlighter
-                pdfDocument={pdfDocument}
-                rotate={rotate}
-                enableAreaSelection={event => event.altKey}
-                onScrollChange={resetHash}
-                scrollRef={scrollTo => {
-                  this.scrollViewerTo = scrollTo;
+                                        this.scrollToHighlightFromHash();
+                                    }}
+                                    onSelectionFinished={(
+                                        position,
+                                        content,
+                                        hideTipAndSelection,
+                                        transformSelection
+                                    ) => (
+                                        <Tip
+                                            onOpen={transformSelection}
+                                            onConfirm={comment => {
+                                                this.addHighlight({content, position, comment});
 
-                  this.scrollToHighlightFromHash();
-                }}
-                onSelectionFinished={(
-                  position,
-                  content,
-                  hideTipAndSelection,
-                  transformSelection
-                ) => (
-                  <Tip
-                    onOpen={transformSelection}
-                    onConfirm={comment => {
-                      this.addHighlight({ content, position, comment });
+                                                hideTipAndSelection();
+                                            }}
+                                        />
+                                    )}
+                                    highlightTransform={(
+                                        highlight,
+                                        index,
+                                        setTip,
+                                        hideTip,
+                                        viewportToScaled,
+                                        screenshot,
+                                        isScrolledTo
+                                    ) => {
+                                        const isTextHighlight = !Boolean(
+                                            highlight.content && highlight.content.image
+                                        );
 
-                      hideTipAndSelection();
-                    }}
-                  />
-                )}
-                highlightTransform={(
-                  highlight,
-                  index,
-                  setTip,
-                  hideTip,
-                  viewportToScaled,
-                  screenshot,
-                  isScrolledTo
-                ) => {
-                  const isTextHighlight = !Boolean(
-                    highlight.content && highlight.content.image
-                  );
+                                        const component = isTextHighlight ? (
+                                            <Highlight
+                                                isScrolledTo={isScrolledTo}
+                                                position={highlight.position}
+                                                comment={highlight.comment}
+                                            />
+                                        ) : (
+                                            <AreaHighlight
+                                                highlight={highlight}
+                                                onChange={boundingRect => {
+                                                    this.updateHighlight(
+                                                        highlight.id,
+                                                        {boundingRect: viewportToScaled(boundingRect)},
+                                                        {image: screenshot(boundingRect)}
+                                                    );
+                                                }}
+                                            />
+                                        );
 
-                  const component = isTextHighlight ? (
-                    <Highlight
-                      isScrolledTo={isScrolledTo}
-                      position={highlight.position}
-                      comment={highlight.comment}
+                                        return (
+                                            <Popup
+                                                popupContent={<HighlightPopup {...highlight} />}
+                                                onMouseOver={popupContent =>
+                                                    setTip(highlight, highlight => popupContent)
+                                                }
+                                                onMouseOut={hideTip}
+                                                key={index}
+                                                children={component}
+                                            />
+                                        );
+                                    }}
+                                    highlights={highlights}
+                                />
+                            )}
+                        </PdfLoader>
+                    </div>
+
+                    <Sidebar
+                        highlights={highlights}
+                        resetHighlights={this.resetHighlights}
+                        rotate={this.rotate}
                     />
-                  ) : (
-                    <AreaHighlight
-                      highlight={highlight}
-                      onChange={boundingRect => {
-                        this.updateHighlight(
-                          highlight.id,
-                          { boundingRect: viewportToScaled(boundingRect) },
-                          { image: screenshot(boundingRect) }
-                        );
-                      }}
-                    />
-                  );
-
-                  return (
-                    <Popup
-                      popupContent={<HighlightPopup {...highlight} />}
-                      onMouseOver={popupContent =>
-                        setTip(highlight, highlight => popupContent)
-                      }
-                      onMouseOut={hideTip}
-                      key={index}
-                      children={component}
-                    />
-                  );
-                }}
-                highlights={highlights}
-              />
-            )}
-          </PdfLoader>
-        </div>
-
-        <Sidebar
-          highlights={highlights}
-          resetHighlights={this.resetHighlights}
-          rotate={this.rotate}
-        />
-      </div>
-    );
-  }
+                </SplitterLayout>
+            </div>
+        );
+    }
 }
 
 export default App;
