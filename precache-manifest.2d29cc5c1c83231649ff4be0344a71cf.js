880efdc28d069c1496ca116c541cb4ed8c2a8105
mikhailz <zisman.mikhail@gmail.com>
2020-06-19 14:01:46 +0300

    Updates

---
+++ precache-manifest.2d29cc5c1c83231649ff4be0344a71cf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4a72dd58a30ea8fb5ced1f1907df13",
+    "url": "/pdf_highlighter/index.html"
+  },
+  {
+    "revision": "fec4daccf42dce5fef8a",
+    "url": "/pdf_highlighter/static/css/2.6f044389.chunk.css"
+  },
+  {
+    "revision": "40e699072b64e798a252",
+    "url": "/pdf_highlighter/static/css/main.765757f6.chunk.css"
+  },
+  {
+    "revision": "fec4daccf42dce5fef8a",
+    "url": "/pdf_highlighter/static/js/2.40456c10.chunk.js"
+  },
+  {
+    "revision": "b02398fe302ba4ef3803015f2c370d5c",
+    "url": "/pdf_highlighter/static/js/2.40456c10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40e699072b64e798a252",
+    "url": "/pdf_highlighter/static/js/main.56fd3b45.chunk.js"
+  },
+  {
+    "revision": "2a07cac83068a19310c7",
+    "url": "/pdf_highlighter/static/js/runtime-main.d34b4dd2.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/pdf_highlighter/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/pdf_highlighter/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/pdf_highlighter/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/pdf_highlighter/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
