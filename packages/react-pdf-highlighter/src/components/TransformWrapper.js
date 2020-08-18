e81fee442b578c86bcc450c89d7109b7db167915
Dmitry Vesnin <dvesnin@exadel.com>
2020-08-18 22:01:21 +0500

    Fixed scale/rotate a bit

--- packages/react-pdf-highlighter/src/components/TransformWrapper.js
+++ packages/react-pdf-highlighter/src/components/TransformWrapper.js
@@ -12,7 +12,6 @@ type State = {
   parent: HTMLElement
 }
 
-// TODO: fix this horror
 class TransformWrapper extends Component<Props, State> {
   constructor(props) {
     super(props);
@@ -38,96 +37,22 @@ class TransformWrapper extends Component<Props, State> {
   }
 
   getContainerTransform(parent: HTMLElement, rotate: number, scale: number): string {
-    if (!parent || parent.style.transform === 'none') {
+    if (!parent) {
       return 'none';
     }
-    const { transform } = parent.style;
-    const rotateMatch = transform.match(/rotate\(-?(\d+)deg\)/);
-    const parentRotate = rotateMatch && +rotateMatch[1] || 0;
-    const scaleMatch = transform.match(/scale\(([\d.]+)/);
-    const parentScale = scaleMatch && +scaleMatch[1] || 1;
     const rect = parent.getBoundingClientRect();
-    const [x, y] = this.getOffsets(rotate, parentRotate, rect) || [0, 0];
-    let finalRot = rotate - parentRotate;
-    if (rotate === -90 && !parentRotate) {
-      finalRot = 90;
-    }
-    if (Math.abs(rotate) === parentRotate) {
-      finalRot = 0;
-    }
-    if (Math.abs(rotate) === 90 && parentRotate === 180) {
-      finalRot = -90;
-    }
-    if (rotate === -90 && parentRotate === 270) {
-      finalRot = 180;
-    }
-    scale /= parentScale;
-    return `rotate(${finalRot}deg) scale(${scale}) translate(${x}px, ${y}px)`;
+    const [x, y] = this.getOffsets(rotate, rect);
+    return `rotate(${rotate}deg) scale(${scale}) translate(${x}px, ${y}px)`;
   }
 
-  getOffsets(rotate: number, parentRotate: number, rect: ClientRect): [number, number] {
-    parentRotate = this.clampRotation(parentRotate);
-    if (!(rotate - parentRotate) && rotate !== -90) {
-      return [0, 0]
-    }
+  getOffsets(rotate: number, rect: ClientRect): [number, number] {
     switch (rotate) {
-      case -180:
-        switch (parentRotate) {
-          case -90:
-            return [-rect.width, 0];
-          case 0:
-            return [-rect.width, -rect.height];
-          case 90:
-            return [0, -rect.height];
-          default:
-            return 0;
-        }
-      case -90:
-        switch (parentRotate) {
-          case -180: case 180:
-            return [-rect.height, 0];
-          case -90:
-            return [-rect.height, -rect.width];
-          case 0:
+      case 90: case -90:
             return [0, -rect.width];
-          default:
-            return 0;
-        }
-      case 0:
-        switch (parentRotate) {
-          case -90:
-            return [-rect.height, 0];
-          case 90:
-            return [-rect.width, 0];
-          case 180:
+      case 180: case -180:
             return [-rect.width, -rect.height];
-          default:
-            return 0;
-        }
-      case 90:
-        switch (parentRotate) {
-          case -90:
-            return [-rect.height, -rect.width];
-          case 0:
-            return [0, -rect.width];
-          case 180:
-            return [-rect.height, 0];
-          default:
-            return 0;
-        }
-      case 180:
-        switch (parentRotate) {
-          case -90:
-            return [-rect.width, 0];
-          case 0:
-            return [-rect.width, -rect.height];
-          case 90:
-            return [0, -rect.height];
-          default:
-            return 0;
-        }
       default:
-        return 0;
+        return [0, 0];
     }
   }
 
