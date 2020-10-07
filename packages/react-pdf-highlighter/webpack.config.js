9bbe23fc5ed534511f41867e4f147324f049d6fb
Kulakov Dmitry <kulakovda@sgaz.pro>
2020-10-07 17:43:20 +0500

    Правка webpack-конфига

--- packages/react-pdf-highlighter/webpack.config.js
+++ packages/react-pdf-highlighter/webpack.config.js
@@ -2,14 +2,14 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   plugins: [new CleanWebpackPlugin()],
   output: {
     publicPath: 'dist/',
     path: path.join(__dirname, "/dist"),
     filename: "pdf-highlighter.js",
-    library: "PdfHighlighter",
+    library: ["c60", "widgets", "pdfAnnotator"],
     libraryTarget: "umd"
   },
   module: {
